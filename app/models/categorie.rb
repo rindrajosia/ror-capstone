676bfc33be25923d0d7b444cf53994b615d358fc
rindra josia <rrindrajosia@gmail.com>
2020-07-13 21:30:11 +0300

    add rubocop

--- app/models/categorie.rb
+++ app/models/categorie.rb
@@ -3,6 +3,6 @@ class Categorie < ApplicationRecord
   has_many :articles, through: :tags, source: 'article'
 
   def self.priority_ordered
-    Categorie.order(priority: :desc).limit(4)
+    Categorie.order(priority: :desc).includes(:articles).limit(4)
   end
 end
