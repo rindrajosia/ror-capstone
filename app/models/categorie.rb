63cc8cd30bc5b7442dd380b2451e173b706b2312
rindra josia <rrindrajosia@gmail.com>
2020-07-17 23:06:55 +0300

    add more test unit model

--- app/models/categorie.rb
+++ app/models/categorie.rb
@@ -1,6 +1,8 @@
 class Categorie < ApplicationRecord
   has_many :relations, foreign_key: 'category_id'
   has_many :articles, through: :relations, source: 'article'
+  validates :name, presence: true
+  validates :priority, presence: true
 
   def self.priority_ordered
     Categorie.ordered_priority.includes(:articles).limit(4)
